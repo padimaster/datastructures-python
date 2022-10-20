389a4acc8c037f395efa08e28d0ae5f0c5591dc7
Alex Padilla <alex.padilla@epn.edu.ec>
2022-10-20 08:52:04 -0500

    recursion

---
+++ ten_common_interview_problems/first_and_last_element.py
@@ -0,0 +1,11 @@
+# Dado un arreglo ordenado y un target, determinar en que posicion del arreglo
+# empieza y termina. Si el target no se encuentra en el arreglo, devolver [-1, -1]
+#
+# Ejemplo
+# Input:   
+#    a = [1,1,2,3,4,5,5,5,5,7]
+#    target = 5
+# Output: [5, 8]
+
+def solve(numbers, target):
+    return [-1, -1]
---
+++ ten_common_interview_problems/kth_largest_element.py
@@ -0,0 +1,18 @@
+# Dado un arreglo de numeros y un entero k, devolver el elemento kth mas grande
+# del arreglo
+
+# Ejemplo:
+# Input:
+#    a = [5,3,6,9,8,7]
+#    target = 5
+# Output: 5
+
+# Explanation:
+# 1st largest element is 9
+# 2nd largest element is 8
+# 3rd largest element is 7
+# 4th largest element is 6
+# 5th largest element is 5
+
+def solve(numbers, k):
+    return 0
